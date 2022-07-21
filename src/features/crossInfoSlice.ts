6ac534a88f78d4e23dfa7333a2fa8c1187e53b27
Londers <semen.morgynenko@gmail.com>
2022-07-21 12:32:42 +0600

    first commit

---
+++ src/features/crossInfoSlice.ts
@@ -0,0 +1,10 @@
+import {CrossControlInfoMsg} from "../common";
+
+
+const initialState: CrossControlInfoMsg = {
+    areaMap: undefined,
+    deviceIP: "",
+    edit: false,
+    history: [],
+    state: undefined
+}
